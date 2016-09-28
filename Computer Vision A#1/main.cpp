fff49d86e4312cf39579950af1ccbd7433fda760
Shehab <shehabmohamed@shehabs-macbook-pro.local>
2016-09-28 12:27:20 +0200

    Initial Commit

---
+++ Computer Vision A#1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Computer Vision A#1
+//
+//  Created by Shehab Mohamed on 9/28/16.
+//  Copyright © 2016 Shehab Mohamed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
